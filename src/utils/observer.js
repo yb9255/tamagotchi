cccb7490aebd976a0a025844c24c64bf694aa663
yb9255 <yoobin9255@gmail.com>
2022-07-13 20:55:23 +0900

    불필요한 코드 제거

--- src/utils/observer.js
+++ src/utils/observer.js
@@ -1,10 +1,9 @@
 import debounce from 'lodash.debounce';
-import loginStyles from '../css/login.css';
-import profileStyles from '../css/profile.css';
+
 import { GROWTH } from '../constants/gameState';
 
 export function observeRoot(controller) {
-  const observer = new MutationObserver(async (entries) => {
+  const observer = new MutationObserver(async () => {
     if (location.pathname === '/' || location.pathname === '/profile') {
       const debouncedPatching = debounce(
         controller.handlePatchingUserInfo.bind(controller),
@@ -15,29 +14,15 @@ export function observeRoot(controller) {
     }
 
     if (controller.router.currentRoute === '/') {
-      const fromLoginPage = entries[0].removedNodes[1].classList.contains(
-        loginStyles.container,
-      );
-
-      const fromProfileBeforeEgg =
-        entries[0].removedNodes[1].classList.contains(
-          profileStyles.container,
-        ) &&
-        controller.gameState.growth !== GROWTH[1] &&
-        controller.gameState.growth !== GROWTH[2];
+      if (controller.currentAnimationFrame) {
+        cancelAnimationFrame(controller.currentAnimationFrame);
+      }
 
       controller.handleSettingMainPage();
       controller.handleSettingNavBar();
       controller.handleMoodImage();
-
-      if (fromLoginPage || fromProfileBeforeEgg) {
-        if (controller.currentAnimationFrame) {
-          cancelAnimationFrame(controller.currentAnimationFrame);
-        }
-
-        controller.gameState.setIdlingState();
-        controller.handleEventsOverTime();
-      }
+      controller.gameState.setIdlingState();
+      controller.handleEventsOverTime();
     } else if (controller.router.currentRoute === '/profile') {
       controller.handleSettingNavBar();
       controller.handleSettingProfilePage();
